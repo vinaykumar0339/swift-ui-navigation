29731583e87bab4439f900bcb36a690d87d6ae63
vinaykumar0339 <vinay9961@gmail.com>
2025-12-25 01:10:09 +0530

    add new way of navigating

--- Sources/swift-ui-navigation/Core/Route.swift
+++ Sources/swift-ui-navigation/Core/Route.swift
@@ -2,61 +2,7 @@
 //  File.swift
 //  swift-ui-navigation
 //
-//  Created by Vinay Kumar on 22/12/25.
+//  Created by Vinay Kumar on 24/12/25.
 //
 
 import Foundation
-
-public struct Route<
-    Screen: ScreenProtocol
->: Hashable {
-    public var id: UUID
-    public let name: Screen
-    private var params: Data?
-    private var options: ScreenOptions?
-    
-    public init<Params: RouteParams>(
-        id: UUID = UUID(),
-        name: Screen,
-        params: Params? = nil,
-        options: ScreenOptions? = nil
-    ) {
-        self.id = id
-        self.name = name
-        self.params = try? JSONEncoder().encode(params)
-        self.options = options
-    }
-    
-    public func getParams<Params: RouteParams>() -> Params? {
-        guard let data = params else { return nil }
-        return try? JSONDecoder().decode(Params.self, from: data)
-    }
-    
-    mutating func updateParams<Params: RouteParams>(_ params: Params) {
-        self.params = try? JSONEncoder().encode(params)
-    }
-    
-    public func getOptions() -> ScreenOptions? {
-        options
-    }
-    
-    mutating func updateOptions(_ options: ScreenOptions) {
-        self.options = options
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-        hasher.combine(name)
-    }
-    
-    public static func == (lhs: Route<Screen>, rhs: Route<Screen>) -> Bool {
-        lhs.id == rhs.id && lhs.name == rhs.name
-    }
-    
-}
-
-public protocol RouteParams: Codable & Hashable {}
-
-public struct EmptyParams: RouteParams {}
-
-public protocol ScreenProtocol: Hashable, CaseIterable {}
